deffb0d2d5480e8c112d1e7793f28d04ef4c0076
coderBi <614666913@qq.com>
2017-05-01 15:55:29 +0800

    修改开机启动命令

--- execBat.vbs
+++ execBat.vbs
@@ -3,5 +3,5 @@
 Set ws = createobject("Wscript.Shell")
 'ws.run "cmd /c start startEXE.bat" '可以执行bat脚本
 'ws.run "execBatFile.exe myStartUp.bat",vbhide '执行exe，并且隐藏执行的窗口
-ws.run "execBatFile.exe myStartUp.bat"
+ws.run "D:\CodeAndRecord\Code\myBat\execBatFile.exe D:\CodeAndRecord\Code\myBat\myStartUp.bat"
 wscript.sleep 1200
--- myStartUp.bat
+++ myStartUp.bat
@@ -1,5 +1,5 @@
 @echo off
-rem 这里放置我自己的开机启动命令
+rem 这里放置我自己的开机启动命令，值得注意的是这里的命令执行的时间要控制好，例如如果这里面一条关闭某个服务的指令在这个服务启动之前就执行了，那么就会出现相应的服务在这个脚本之后启动了。所以这个脚本应该控制在其他服务后面执行。
 
 rem 关闭mysql, net stop 用于开启本地的一个服务
 net stop mysql
@@ -7,6 +7,15 @@ rem 启动wifi
 netsh wlan start hostednetwork
 rem 关闭Apache
 httpd -k stop
+
+rem 关闭mongodb
+net stop MongoDB
+
+rem 关闭sphinx
+net stop searchd
+
+rem 关闭memcache
+net stop "memcached Server"
 
 rem 退出
 exit
